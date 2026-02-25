7c858e44ff16c33977dd614365e4fb096bd8eee2
han0gu <imgum9@gmail.com>
2026-02-26 01:17:32 +0900

    try 3

--- hash/phone-number-list/hsw/solution.py
+++ hash/phone-number-list/hsw/solution.py
@@ -1,28 +1,18 @@
 def solution(phone_book):
-    for idx in range(len(phone_book) - 1):
-        num1 = phone_book[idx]
+    if len(phone_book) == 1:
+        return True
 
-        for jdx in range(idx + 1, len(phone_book)):
-            num2 = phone_book[jdx]
+    phone_book.sort(key=int)
 
-            print(f"{num1}, {num2}: {is_dup(num1, num2)}")
-            if is_dup(num1, num2):
+    d = {phone_book[0]: 1}
+
+    for phone in phone_book[1:]:
+        for i in range(1, len(phone)):
+            target = phone[:i]
+
+            if d.get(target):
                 return False
 
+        d[phone] = 1
+
     return True
-
-
-def is_dup(num1: str, num2: str):
-    len1: int = len(num1)
-    len2: int = len(num2)
-
-    if len1 < len2:
-        return num2.startswith(num1)
-    elif len1 > len2:
-        return num1.startswith(num2)
-    else:
-        return num1 == num2
-
-
-phone_book = ["119", "97674223", "1195524421"]
-solution(phone_book)
