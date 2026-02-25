596c7f20f266ca1866efa162221f534a2e93747c
이영기 <at.oz.wizard@gmail.com>
2026-02-25 21:45:45 +0900

    전화번호부 풀이 끝

--- hash/phone-number-list/yyk/solution.py
+++ hash/phone-number-list/yyk/solution.py
@@ -1,3 +1,14 @@
 def solution(phone_book):
-    answer = True
-    return answer
+    
+# 주어진 문자열 소팅, i+1 이 i로 시작하는지!
+    phone_book = sorted(phone_book)
+    
+    for i in range(len(phone_book)-1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
+        # if phone_book[i+1].startswith(phone_book[i]) is bool or True:
+        #     return True
+        # print(phone_book)
+        # False 만 나오면 나머지는 True 이어야 하므로
+        # 다 필요없고, return True
+    return True
