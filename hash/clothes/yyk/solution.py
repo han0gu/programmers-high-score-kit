1f53bfa9df951a84bf1669cf7415cb9f7be637a5
이영기 <at.oz.wizard@gmail.com>
2026-03-05 09:30:48 +0900

    의상 풀이, 아쉬움, 프로덕트로 해보고싶음

---
+++ hash/clothes/yyk/solution.py
@@ -0,0 +1,32 @@
+from itertools import product
+
+def solution(clothes):
+    
+    
+    # [["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]
+
+    # 리스트 안에 리스트형태, 그룹지어 매핑
+
+    def mk_grouped_dict(clothes):
+        mapping = {}
+        for item in clothes:
+            key = item[1] #부위를 키로
+            value = item[0] #의상을 밸류로
+            if key in mapping:
+                mapping[key].append(value)
+
+            else: 
+                mapping[key] = [value]
+
+        return mapping
+    
+    
+    def total_combinations(clothes):
+        answer = 1
+        for i in mk_grouped_dict(clothes).keys():
+            clothes_count = len(mk_grouped_dict(clothes)[i])
+            answer *= (clothes_count + 1)
+        return answer - 1
+        #딕셔너리 키의 값을 +1 한것들을 모두 곱 -1
+
+    return total_combinations(clothes)
