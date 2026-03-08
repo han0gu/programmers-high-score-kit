c1f8f0ed7a0d715ce49c1a6e462cc39915909d1a
han0gu <imgum9@gmail.com>
2026-03-09 01:27:49 +0900

    try 2:  Pythonize

--- hash/best-album/hsw/soultion.py
+++ hash/best-album/hsw/soultion.py
@@ -1,4 +1,4 @@
-from functools import cmp_to_key
+from collections import defaultdict
 from typing import TypedDict
 
 
@@ -17,38 +17,27 @@ def solution(genres, plays):
     """
     2. 타입에 맞게 원본 데이터 가공
     """
-    dd = {}
-    for genre in set(genres):
-        dd[genre] = {"total": 0, "songs": []}
-
-    for idx, genre in enumerate(genres):
-        dd[genre]["total"] += plays[idx]
-        dd[genre]["songs"].append((idx, plays[idx]))
+    dd = defaultdict(lambda: {"total": 0, "songs": []})
+    for idx, (genre, play) in enumerate(zip(genres, plays)):
+        dd[genre]["total"] += play
+        dd[genre]["songs"].append((idx, play))
 
     """
     3. 필요한 순서대로 정렬
     """
     sorted_by_total = dict(
-        sorted(dd.items(), key=cmp_to_key(compare_genre), reverse=True)
+        sorted(dd.items(), key=lambda x: x[1]["total"], reverse=True)
     )
 
     for value in sorted_by_total.values():
-        songs = value["songs"]
-        sorted_by_plays = sorted(songs, key=cmp_to_key(compare_songs), reverse=True)
+        sorted_by_plays = sorted(value["songs"], key=lambda x: x[1], reverse=True)
         answer.extend([t[0] for t in sorted_by_plays[:2]])
 
     return answer
 
 
-def compare_genre(g1: tuple[str, GenreType], g2: tuple[str, GenreType]) -> int:
-    return g1[1]["total"] - g2[1]["total"]
-
-
-def compare_songs(s1: tuple[int, int], s2: tuple[int, int]) -> int:
-    return s1[1] - s2[1]
-
-
-genres = ["classic", "pop", "classic", "classic", "pop"]
-plays = [500, 600, 150, 800, 2500]
-answer = solution(genres, plays)
-print("answer:", answer)
+if __name__ == "__main__":
+    genres = ["classic", "pop", "classic", "classic", "pop"]
+    plays = [500, 600, 150, 800, 2500]
+    answer = solution(genres, plays)
+    print("answer:", answer)
